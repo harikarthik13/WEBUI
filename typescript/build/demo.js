19192eba2ef21d11115f8ed50d7d48057e5985cf
harikarthik13 <harikarthik1301@gmail.com>
2023-02-27 15:04:42 +0530

    ts task2

--- typescript/build/demo.js
+++ typescript/build/demo.js
@@ -1,1 +1,15 @@
 "use strict";
+class Employee {
+    constructor(empid, empname, empdesignation, empsalary, empproject) {
+        this.empid = empid;
+        this.empname = empname;
+        this.empdesignation = empdesignation;
+        this.empsalary = empsalary;
+        this.empproject = empproject;
+    }
+    getEmpdetails() {
+        console.log(`empid :${this.empid}  empname :${this.empname}  empdesignation :${this.empdesignation}  empsalary :${this.empsalary} `);
+    }
+}
+const emp = new Employee(1, "Hari", "developer", 30000, "webui");
+emp.getEmpdetails();
--- typescript/src/demo.ts
+++ typescript/src/demo.ts
@@ -1,7 +1,24 @@
-class Employee{
-empid? : number|string;
-empname: string;
-empdesignation : string;
-empsalary? : number|string;
-empproject : string;
+class Employee {
+    empid?: number | string;
+    empname: string;
+    empdesignation: string;
+    empsalary: number;
+    empproject?: string;
+
+    public constructor(empid: number | string,empname: string, empdesignation: string, empsalary: number, empproject: string)
+
+    {
+
+        this.empid = empid;
+        this.empname = empname;
+        this.empdesignation = empdesignation;
+        this.empsalary = empsalary;
+        this.empproject = empproject;
+
+    }
+    getEmpdetails(){
+        console.log(`empid :${this.empid}  empname :${this.empname}  empdesignation :${this.empdesignation}  empsalary :${this.empsalary}  empproject :${this.empproject}`)
+    }
 }
+    const emp = new Employee(1,"Hari","developer",30000,"webui");
+    emp.getEmpdetails();
